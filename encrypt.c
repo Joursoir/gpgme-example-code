df865a0e42c2d3331b08ccfa05e6b2fc756e69a8
Joursoir <chat@joursoir.net>
2021-02-24 13:10:35 +0000

    print info about invalid keys

--- encrypt.c
+++ encrypt.c
@@ -12,6 +12,8 @@ int main()
 	gpgme_error_t err;	
 	gpgme_ctx_t ctx;
 	gpgme_data_t plain, cipher;
+	gpgme_key_t key[] = { NULL, NULL }; /* must be null-terminated! */
+	gpgme_encrypt_result_t result;
 	init_gpgme();
 
 	err = gpgme_new(&ctx);
@@ -25,13 +27,21 @@ int main()
 	err = gpgme_data_new(&cipher);
 	exit_if_err(err);
 
-	gpgme_key_t key[] = { NULL, NULL }; /* must be null-terminated! */
 	err = gpgme_get_key(ctx, gpg_key, &key[0], 0);
 	exit_if_err(err);
 
 	err = gpgme_op_encrypt(ctx, key, GPGME_ENCRYPT_ALWAYS_TRUST,
 		plain, cipher);
 	exit_if_err(err);
+	result = gpgme_op_encrypt_result(ctx);
+	while(result->invalid_recipients) {
+		fprintf(stderr, "Invalid recipient: %s\n"
+			"%s: %s\n",
+			result->invalid_recipients->fpr,
+			gpgme_strsource(result->invalid_recipients->reason),
+			gpgme_strerror(result->invalid_recipients->reason));
+		result->invalid_recipients = result->invalid_recipients->next;
+	}
 	loop_read(cipher);
 	
 	gpgme_data_release(plain);
